9615411a2f2206177f40fa86ed86d875713f7b72
Kian Sweeney <73000700+kiansweeney11@users.noreply.github.com>
2023-01-25 22:12:30 +0000

    side-by-side

--- time-series-analysis/time-series-analysis-leeds-s1s2.r
+++ time-series-analysis/time-series-analysis-leeds-s1s2.r
@@ -9,13 +9,35 @@ library(RcppRoll)
 library(lubridate)
 library(readr)
 library(plotly)
+library(gridExtra)
+library(patchwork)
 
 ## read data
-data = read_csv("r-data/less-headers.csv")
+file1 <- "less-headers.csv"
+dir <- "r-data/"
+
+data = read_csv(paste0(dir,file1))
+
+file2 <- "less-headers-2122-lee.csv"
+
+data1 = read_csv(paste0(dir,file2))
 
 # selected columns for analysis
 data_select = data %>%
   select(c(1,5,6,7,8,9,10))
+### s2 data
+data_select_s2 = data1 %>%
+  select(c(1,5,6,7,8,9,10))
+
+data_rcpp_s2 = data_select_s2 %>%
+  mutate(xG_rolling_5_game_avg = movavg(xG, 5, type="s"),
+         xGA_rolling_5_game_avg = movavg(xGA, 5, type="s"))
+
+data_xg_actual_s2 = data_rcpp_s2 %>%
+  mutate(GF_rolling_5_game_avg = movavg(GF, 5, type="s"),
+         GA_rolling_5_game_avg = movavg(GA, 5, type="s"))
+
+######
 
 # rolling avg
 # use pracma library (pracma) as this doesnt allow for zero values
@@ -78,8 +100,25 @@ plot_against_xGA = data_xg_actual %>%
   ylab("Goals") + 
   scale_color_manual(values = c(GA = "blue", xGA = "red"),
                      labels = c(GA = "Goals Against", xGA = "xGA"))
-  #theme_classic()
-ggplotly(plot_against_xGA, tooltip = c("GA", "xGA"))
 
-#plot_against_xGA
+plt1 <- ggplotly(plot_against_xGA, tooltip = c("x", "GA", "xGA"))
+
+## season 2
+
+plot_against_xGA_s2 = data_xg_actual_s2 %>%
+  ggplot(aes(x = Date, GA = GA_rolling_5_game_avg, xGA = xGA_rolling_5_game_avg)) +
+  geom_line(aes(y = GA_rolling_5_game_avg, colour="GA")) +
+  geom_line(aes(y = xGA_rolling_5_game_avg, colour="xGA")) +
+  ggtitle("5 Point MA Leeds United xGA VS GA 2021/22") +
+  ylab("Goals") + 
+  scale_color_manual(values = c(GA = "blue", xGA = "red"),
+                     labels = c(GA = "Goals Against", xGA = "xGA"))
+
+plt2 <- ggplotly(plot_against_xGA_s2, tooltip = c("x", "GA", "xGA"))
+
+fig <- subplot(plt1, plt2) %>% 
+  layout(title = 'Leeds United S1 VS S2 xGA/GA rolling Averages Comparison')
+fig
+
+
 
